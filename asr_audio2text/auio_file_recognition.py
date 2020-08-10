6f3bec103fe6123f4d792994c74d4324a8198722
Solomon Xie <xiaoboxie@appannie.com>
2020-08-10 19:37:36 +0800

    successfully translated

--- asr_audio2text/auio_file_recognition.py
+++ asr_audio2text/auio_file_recognition.py
@@ -1,25 +1,74 @@
+import json
 import base64
+from time import sleep
 
 from tencentcloud.asr.v20190614 import models
 
 from common.auth import get_asr_auth_client
 
 
-def recognize_audio_file(filepath):
-    with open(filepath, 'rb') as f:
-        data = f.read()
-        data_len = len(data)
-        base64_wav = base64.b64encode(data).decode()
+class AudioRecognition:
 
-    req = models.CreateRecTaskRequest()
-    params = {
-        "EngineModelType": "16k_0",
-        "ChannelNum": 1,
-        "ResTextFormat": 0,
-        "SourceType": 1,
-        "Data": base64_wav,
-        "DataLen": data_len,
-    }
-    req._deserialize(params)
-    resp = get_asr_auth_client().CreateRecTask(req)
-    print(resp.to_json_string())
+    def __init__(self):
+        self.client = get_asr_auth_client()
+
+    def process_from_file(self, filepath):
+        task = self.create_task_from_file(filepath)
+        status = 0
+        result = self.query_task(task['Data']['TaskId'])
+        return result
+
+    def create_task_from_file(self, filepath: str) -> dict:
+        """
+        Reply: {"Data": {"TaskId": 856184969}, "RequestId": "c7e76487-64bc-45b0-95da-756f7eb928d1"}
+        """
+        with open(filepath, 'rb') as f:
+            data = f.read()
+            data_len = len(data)
+            base64_wav = base64.b64encode(data).decode()
+
+        req = models.CreateRecTaskRequest()
+        params = {
+            "EngineModelType": "16k_0",
+            "ChannelNum": 1,
+            "ResTextFormat": 0,
+            "SourceType": 1,
+            "Data": base64_wav,
+            "DataLen": data_len,
+        }
+        req._deserialize(params)
+        resp = self.client.CreateRecTask(req)
+        reply = json.loads(resp.to_json_string())
+        return reply
+
+    def query_task(self, task_id: int) -> dict:
+        status = 0
+        while status in [0, 1]:
+            print('Fetching result...')
+            result = self.query_task_once(task_id)
+            print('\t', result)
+            status = result['Data']['Status']
+            sleep(5)
+        print('Fetched result:')
+        return result
+
+    def query_task_once(self, task_id: int) -> dict:
+        """
+        Reply:
+        {
+            "Data": {
+                "Status": 2,
+                "StatusStr": "success",
+                "Result": "[0:0.000,0:1.540]  \u6885\u8d5b\u5fb7\u65af\u5954\u9a70\u3002\n",
+                "TaskId": 737083613,
+                "ErrorMsg": "",
+                "ResultDetail": null
+            },
+            "RequestId": "4f87d14f-8b0f-4970-b517-eaf8c5df2e9d"
+        }
+        """
+        req = models.DescribeTaskStatusRequest()
+        req.from_json_string(json.dumps({'TaskId': task_id}))
+        resp = self.client.DescribeTaskStatus(req)
+        reply = json.loads(resp.to_json_string())
+        return reply
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from asr_audio2text.auio_file_recognition import recognize_audio_file
+from asr_audio2text.auio_file_recognition import AudioRecognition
 
 
 def main():
-    path = './samples/sample.mp3'
-    recognize_audio_file(path)
+    result = AudioRecognition().process_from_file('./samples/sample.mp3')
+    print(result)
 
 
 if __name__ == '__main__':
