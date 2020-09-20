d9fb1e349634762adf84e54346f04482ae50b154
Solomon Xie <xiaoboxie@appannie.com>
2020-09-20 17:56:22 +0800

    add xunfei api

--- settings.py
+++ settings.py
@@ -1,8 +1,13 @@
 import os
 
+# TENCENT
 TENCENT_REGION = 'ap-beijing'
 TENCENT_SECRET_ID = ''
 TENCENT_SECRET_KEY = ''
+
+# Xunfei
+XUNFEI_APPID = ''
+XUNFEI_ASR_SECRETKEY = ''
 
 if os.path.exists('settings_local.py'):
     from settings_local import *  # NOQA: F401,F403
---
+++ xunfei/asr/README.md
@@ -0,0 +1,2 @@
+# 讯飞语音转写
+Refer to: https://www.xfyun.cn/doc/asr/lfasr/API.html
