71e93b4a180401a7dc108dcfe8d73f0017175452
Solomon Xie <xiaoboxie@appannie.com>
2020-09-20 17:48:40 +0800

    rename to cloud_api: not limited to tencent

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 import os
 
-REGION = 'ap-beijing'
-SECRET_ID = ''
-SECRET_KEY = ''
+TENCENT_REGION = 'ap-beijing'
+TENCENT_SECRET_ID = ''
+TENCENT_SECRET_KEY = ''
 
 if os.path.exists('settings_local.py'):
     from settings_local import *  # NOQA: F401,F403
